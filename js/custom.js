016010eaff454983ee2073b1ebde83f17ab79ddb
cduyzh <450311265@qq.com>
2017-06-23 11:31:55 +0800

    项目前后整合V2.0
    1.完成后台所有页面布局和功能交互模块开发并整合到前台项目中
    2.完成后自适应屏幕，兼容苹果5，6以上的任何屏幕尺寸包括平板
    3.修改了美工提出的前台页面留言的背景图和文字排版

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
   /* Back to Top
-                                  -----------------------------------------------*/
+                                      -----------------------------------------------*/
   $(window).scroll(function() {
       if ($(this).scrollTop() > 200) {
           $('.go-top').fadeIn(200);
@@ -237,3 +237,42 @@
       }
 
   })(window);
+
+
+  /*!
+   * Propeller v1.1.0 (http://propeller.in): radio.js
+   * Copyright 2016-2017 Digicorp, Inc.
+   * Licensed under MIT (http://propeller.in/LICENSE)
+   */
+
+  $(document).ready(function() {
+      $('.pmd-radio input').after('<span class="pmd-radio-label">&nbsp;</span>');
+      //-- Radio Ripple Effect --//
+      $(".pmd-radio-ripple-effect").on('mousedown', function(e) {
+          var rippler = $(this);
+          $('.ink').remove();
+          // create .ink element if it doesn't exist
+          if (rippler.find(".ink").length === 0) {
+              rippler.append('<span class="ink"></span>');
+          }
+          var ink = rippler.find(".ink");
+          // prevent quick double clicks
+          ink.removeClass("animate");
+          // set .ink diametr
+          if (!ink.height() && !ink.width()) {
+              // var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
+              ink.css({ height: 15, width: 15 });
+          }
+          // get click coordinates
+          var x = e.pageX - rippler.offset().left - ink.width() / 2;
+          var y = e.pageY - rippler.offset().top - ink.height() / 2;
+          // set .ink position and add class .animate
+          ink.css({
+              top: y + 'px',
+              left: x + 'px'
+          }).addClass("animate");
+          setTimeout(function() {
+              ink.remove();
+          }, 1500);
+      })
+  })
